1b6141477ca5d2ee71360d799d68c18f8f408a09
shivaniii <anumulashivani94@gmail.com>
2016-09-27 15:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one', function (req, res) {
+    res.sendFile("This servers the first article.")
+});
+
+app.get('article-two', function (req, res) {
+    res.sendFile("This serves article two.")
+});
+
+ap.get('article three', function (req, res) {
+    res.sendFile("This serves article three.")
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
