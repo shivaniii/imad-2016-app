b0b5077c02517cd7c06e755d3d81a2ec07da6b33
shivaniii <anumulashivani94@gmail.com>
2016-09-30 16:36:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ var articles = {
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
+    var heading = data.heading;
     var content = data.content;
     
 var htmlTemplate =
