03b1e8e1029786beb0b69a0324b3acfd0066f49b
shivaniii <anumulashivani94@gmail.com>
2016-10-18 15:20:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // change the text of the main-text div
-var element = document.getElementById('Anyvasoft'
+var element = document.getElementById('anyvasoft-img'
 );
 
 element.innerHTML = 'Anyvasoft';
