abf5b36e2ced000b63977a5719a42b5064517df0
shivaniii <anumulashivani94@gmail.com>
2016-10-18 21:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.use(morgan('combined'));
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
