24ef31aee8db400bea9c64a71e641c6b348c9be8
shivaniii <anumulashivani94@gmail.com>
2016-10-20 14:54:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // change the text of the main-text div
-var element = document.getElementById('main-text'
+var element = document.getElementById(''
 );
 
-element.innerHTML = 'Anyvasoft';
+element.innerHTML = '';
