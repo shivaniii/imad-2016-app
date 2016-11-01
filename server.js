f5c8f483e4789ecb0f2ca2b2b82a7c01b800621c
shivaniii <anumulashivani94@gmail.com>
2016-11-01 12:05:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/articles/:articleName', function (req, res) {
-    //articleNae == article-one
+    //articleName == article-one
     //articles(articleName) =={} content object for article-one
- pool.query("SELECT * FROM artcle WHERE title =" + req.params.articleName, function (err, result){
+ pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function (err, result){
      if (err) {
          res.status(500).send(err.toString());
      } else {
