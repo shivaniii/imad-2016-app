468cacac7eb43694d7085379884db920faf8f74c
shivaniii <anumulashivani94@gmail.com>
2016-11-07 01:37:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,14 +55,16 @@ function createTemplate (data)
 
 app.get('/my-interests.html', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'my-interests.html'));
-})
+});
 
 app.get('/about-me.html', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'about-me.html'));
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
