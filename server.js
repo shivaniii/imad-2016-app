4c8a56add5f49357207044487dfe39c3b690951c
shivaniii <anumulashivani94@gmail.com>
2016-11-07 20:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,8 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});;
+});
+
 app.get('/articles/:articleName', function (req, res) {
     //articleName == article-one
     //articles(articleName) =={} content object for article-one
