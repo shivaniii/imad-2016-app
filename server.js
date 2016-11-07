a60849570c004732001c82f6da8f574188c02544
shivaniii <anumulashivani94@gmail.com>
2016-11-07 21:31:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var config = {
+    user: 'shivaniii',
+    database: 'shivaniii',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+var app = express();
+app.use(morgan('combined'));
+           
 var articles = {
 'article-one' : {
    title : 'Internet | Anumula Shivani',
