ac5c2b5e8e418d35d531ade62c69c33f567b55d4
shivaniii <anumulashivani94@gmail.com>
2016-11-07 21:15:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,9 +96,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function (req, res) {
+    //articleName == article-one
+    //articles(articleName) =={} content object for article-one
+    //SELECT * FROM article WHERE title = 'article-one'
+ pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         if (result.rows.length === 0) {
+             res.status(404).send('Article not found');
+     } else {
+        var articleData= result.rows[0];
+        res.sendFile(createTemplate(articleData));
+     }}
+ });
 });
 
 var pool = new Pool(config);
