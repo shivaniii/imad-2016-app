238258756378c90c46e5ba78af5c747ba3d65a52
shivaniii <anumulashivani94@gmail.com>
2016-11-07 18:23:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,54 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 
+var articleOne = {
+   title : 'Internet | Anumula Shivani',
+   heading : 'Internet',
+   date : 'Nov 7, 2016',
+   content :` <p>The Internet is the global system of interconnected computer networks that use the Internet protocol suite (TCP/IP) to link devices worldwide. It is a network of networks that consists of private, public, academic, business, and government networks of local to global scope, linked by a broad array of electronic, wireless, and optical networking technologies. The Internet carries an extensive range of information resources and services, such as the inter-linked hypertext documents and applications of the World Wide Web (WWW), electronic mail, telephony, and peer-to-peer networks for file sharing.</p>
+
+<p> The origins of the Internet date back to research commissioned by the United States federal government in the 1960s to build robust, fault-tolerant communication via computer networks.[1] The primary precursor network, the ARPANET, initially served as a backbone for interconnection of regional academic and military networks in the 1980s. The funding of the National Science Foundation Network as a new backbone in the 1980s, as well as private funding for other commercial extensions, led to worldwide participation in the development of new networking technologies, and the merger of many networks.[2] The linking of commercial networks and enterprises by the early 1990s marks the beginning of the transition to the modern Internet,[3] and generated a sustained exponential growth as generations of institutional, personal, and mobile computers were connected to the network. Although the Internet was widely used by academia since the 1980s, the commercialization incorporated its services and technologies into virtually every aspect of modern life.</p>
+
+<p>Internet use grew rapidly in the West from the mid-1990s and from the late 1990s in the developing world.[4] In the 20 years since 1995, Internet use has grown 100-times, measured for the period of one year, to over one third of the world population.[5][6] Most traditional communications media, including telephony, radio, television, paper mail and newspapers are being reshaped or redefined by the Internet, giving birth to new services such as email, Internet telephony, Internet television music, digital newspapers, and video streaming websites. Newspaper, book, and other print publishing are adapting to website technology, or are reshaped into blogging, web feeds and online news aggregators. The entertainment industry was initially the fastest growing segment on the Internet.[citation needed] The Internet has enabled and accelerated new forms of personal interactions through instant messaging, Internet forums, and social networking. Online shopping has grown exponentially both for major retailers and small businesses and entrepreneurs, as it enables firms to extend their "bricks and mortar" presence to serve a larger market or even sell goods and services entirely online. Business-to-business and financial services on the Internet affect supply chains across entire industries.
+
+<p>The Internet has no centralized governance in either technological implementation or policies for access and usage; each constituent network sets its own policies.[7] Only the overreaching definitions of the two principal name spaces in the Internet, the Internet Protocol address space and the Domain Name System (DNS), are directed by a maintainer organization, the Internet Corporation for Assigned Names and Numbers (ICANN). The technical underpinning and standardization of the core protocols is an activity of the Internet Engineering Task Force (IETF), a non-profit organization of loosely affiliated international participants that anyone may associate with by contributing technical expertise.[8]' </p> `
+   
+};
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemplate = `
+<html>
+<head>
+<title>
+${title}
+</title>
+<meta name="viewport" content="width-device-width", initial-scale-1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+<div class="container">
+<div>
+<a href='/'>Home</a>
+</div>
+<hr/>
+<h3>
+${heading}
+</h3>
+<div>
+${date}
+</div>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
 var config = {
     user: 'shivaniii',
     database: 'shivaniii',
@@ -19,6 +67,9 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/aricle-one', function (req, res) {
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/articles/:articleName', function (req, res) {
