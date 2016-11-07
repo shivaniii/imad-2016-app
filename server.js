8a13b9af8f907b44986649cf4417835b14efa678
shivaniii <anumulashivani94@gmail.com>
2016-11-07 21:27:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,22 +96,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articles/:articleName', function (req, res) {
-    //articleName == article-one
-    //articles(articleName) =={} content object for article-one
-    //SELECT * FROM article WHERE title = 'article-one'
- pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
-     if (err) {
-         res.status(500).send(err.toString());
-     } else {
-         if (result.rows.length === 0) {
-             res.status(404).send('Article not found');
-     } else {
-        var articleData= result.rows[0];
-        res.sendFile(createTemplate(articleData));
-     }}
- });
-});
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
@@ -129,6 +113,22 @@ app.get('/test-db', function (req, res) {
 
 app.get('/about-me.html', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'about-me.html'));
+});
+app.get('/articles/:articleName', function (req, res) {
+    //articleName == article-one
+    //articles(articleName) =={} content object for article-one
+    //SELECT * FROM article WHERE title = 'article-one'
+ pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err, result){
+     if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         if (result.rows.length === 0) {
+             res.status(404).send('Article not found');
+     } else {
+        var articleData= result.rows[0];
+        res.sendFile(createTemplate(articleData));
+     }}
+ });
 });
 
 app.get('/ui/style.css', function (req, res) {
