fdfdd9b624f1d3250b77ec82742fe7df17b436a2
shivaniii <anumulashivani94@gmail.com>
2016-11-09 15:50:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/articles/:articleName', function (req, res) {
              res.status(404).send('Article not found');
      } else {
         var articleData= result.rows[0];
-        res.sendFile(createTemplate(articleData));
+        res.send(createTemplate(articleData));
      }
          
      }
