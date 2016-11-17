9280e262bf68d700e192fd26631234f88d469a55
shivaniii <anumulashivani94@gmail.com>
2016-11-17 20:52:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,8 @@ return htmlTemplate;
 
 function hash (input, salt) {
     // how do we create a hash?
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'shah512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
-    
 }
 app.get('/hash/:input', function (req, res){
 var hashedString = hash(req.params.input, 'this-is-some-random-string');
