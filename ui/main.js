8d8ef1cf59d0ba61443bdc48b2314e8f07312bca
shivaniii <anumulashivani94@gmail.com>
2016-11-19 04:19:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var submit = document.getElementById("submit_btn");
-submit_btn.onclick = function() {
+submit.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
      if (request.readyState === XMLHttpRequest.DONE) {
