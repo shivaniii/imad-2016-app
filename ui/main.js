1a6b111b6ac5c266d4dbb7dad71ee0fc27bd120c
shivaniii <anumulashivani94@gmail.com>
2016-11-18 21:47:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,16 +4,14 @@ submit.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
      if (request.readyState === XMLHttpRequest.DONE) {
-         if (request.status === 200) {
-             var names = request.responseText;
-             names = JSON.parse(names);
-             var list = '';
-             for (var i=0; i< names.length; i++) {
-                 list == '<li>' + names[i] + '<li>';
-             }
-             var uI = document.getElementById('namelist');
-             uI.InnerHTML = list ;
-         }}};
+         if (request.status === 200) ;
+           alert('Logged in successfully');
+         } else if (request.status === 403){
+             alert('Username/Password is incorrect!');
+         } else if (request.status === 500) {
+             alert('Something went wrong on the server');
+         
+     }};
                  var username = document.getElementById('username').value;
                  var password = document.getElementById('password').value;
                  console.log(username);
