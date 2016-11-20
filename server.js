f154ea23373afa30787240f24c054f5e3aca1d94
shivaniii <anumulashivani94@gmail.com>
2016-11-20 23:38:16 +0530

    Local commits

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ var articles = {
     <img id="html imge" src="http://fflib.librarymarket.com/sites/default/files/content/events/html-file-extension-interface-symbol_318-45345.png" />
 
 <p>HTML can embed programs written in a scripting language such as JavaScript which affect the behavior and content of web pages. Inclusion of CSS defines the look and layout of content. The World Wide Web Consortium (W3C), maintainer of both the HTML and the CSS standards, has encouraged the use of CSS over explicit presentational HTML since 1997. </p>`
-};
+}};
       
 function createTemplate (data) {
     var title = data.title;
