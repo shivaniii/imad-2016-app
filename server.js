8a39a6a796ed762c9ec0e438cecd8ab3724b59da
shivaniii <anumulashivani94@gmail.com>
2016-11-21 00:16:03 +0530

    Local commits

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(bodyParser.json());
 app.use(session({
 secret : 'someRandomSecretValue',
 cookie : { maxAge: 1000 * 60 *60 * 24* 30}
+store: sessionStore,store // connect-mongo session store
+proxy: true,
+resave: true,
+saveUninitialized: true
 }));
            
 var articles = {
