9e9df828ffef5b0e7f5ba8c2959b02b18b57d754
shivaniii <anumulashivani94@gmail.com>
2016-11-20 19:12:20 +0530

    Local changes

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,15 @@
 console.log('Loaded!');
-var submit = document.getElementById("submit_btn");
+function LoginForm () {
+var loginHtml =
+<h3> Login to comment on my articles </h3>
+<input type="text" id="username" placeholder="username" />
+<input type="text" id="password" placeholder="******" />
+<br></br>
+<input type="submit" id="login_btn" value="Login" />
+;
+document.getElementById('login_area').innerHTML = 'loginHtml';
+
+var submit = document.getElementById("login_btn");
 submit.onclick = function() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
@@ -10,8 +20,10 @@ submit.onclick = function() {
              alert('Username/Password is incorrect!');
          } else if (request.status === 500) {
              alert('Something went wrong on the server');
-         }
+submit.value = 'Login';
+         } loadLogin();
      }};
+
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
@@ -19,4 +31,82 @@ console.log(password);
 request.open('POST', 'http://shivaniii.imad.hasura-app.io/login' , true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
+submit.value = 'Logging in.. Please wait....'};
+
+var register = document.getElementById('register_btn');
+register.onclick = function () {
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+if (request.readyState === XMLHttpRequest.DONE) {
+if (request.status === 200) {
+alert('Registered Successfully!');
+register.value = 'User Registered!';
+} else {
+alert('Couldn't register the user');
+register.value = 'Register';
+}}};
+
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', '/create-user', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));  
+register.value = 'Registering User...';
+    
+    };
+}
+function loadLoggedInUser (username) {
+var loginArea = document.getElementById('login_area');
+loginArea.innerHTML =
+<h3> Welcome <i>${username}</i></h3>
+<a href="/logout">Logout</a>
+;
+}
+
+function loadLogin () {
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+if (request.readyState === XMLHttpRequest.DONE) {
+if (request.status === 200) {
+loadLoggedInUser(this.responseText);
+} else {
+loadLoginForm();
+}
+}
 };
+request.open('GET", '/check-login', true);
+request.send(null);
+}
+
+function loadArticles () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+
+loadLogin();
+
+loadArticles();
