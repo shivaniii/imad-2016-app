3b675d26af4236606234229c5184186f0754b34e
shivaniii <anumulashivani94@gmail.com>
2016-11-21 00:36:44 +0530

    Local commits

--- ui/main.js
+++ ui/main.js
@@ -19,4 +19,5 @@ console.log(password);
 request.open('POST', 'http://shivaniii.imad.hasura-app.io/login' , true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
+};
 
