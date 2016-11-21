f8f6bed5088e602b1eef0b2bf3001c8d36db8288
shivaniii <anumulashivani94@gmail.com>
2016-11-21 15:11:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,5 @@ console.log(password);
 request.open('POST', 'http://shivaniii.imad.hasura-app.io/login' , true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
-};
+
 
