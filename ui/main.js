26b037272897a292de573a711e6464bef9ae7ed8
shivaniii <anumulashivani94@gmail.com>
2016-11-23 20:56:17 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -10,16 +10,15 @@ function loadLoginForm () {
 
     document.getElementById('login_area').innerHTML = loginHtml;
     
-    // Submit username/password to login
     var submit = document.getElementById('login_btn');
     submit.onclick = function () {
-        // Create a request object
+       
         var request = new XMLHttpRequest();
         
-        // Capture the response and store it in a variable
+       
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
+              
               if (request.status === 200) {
                   submit.value = 'Login successful';
               } else if (request.status === 403) {
@@ -33,10 +32,9 @@ function loadLoginForm () {
               }
               loadLogin();
           }  
-          // Not done yet
-        };
         
-        // Make the request
+        };
+      
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
@@ -50,13 +48,13 @@ function loadLoginForm () {
     
     var register = document.getElementById('register_btn');
     register.onclick = function () {
-        // Create a request object
+      
         var request = new XMLHttpRequest();
         
-        // Capture the response and store it in a variable
+       
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
+             
               if (request.status === 200) {
                   alert('User created successfully');
                   register.value = 'Registered!';
@@ -67,7 +65,6 @@ function loadLoginForm () {
           }
         };
         
-        // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
         console.log(username);
@@ -82,7 +79,7 @@ function loadLoginForm () {
 
 
 function loadLogin () {
-    // Check if the user is already logged in
+   
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -105,7 +102,7 @@ function loadLogin () {
     `;
 }
 function loadArticles () {
-        // Check if the user is already logged in
+       
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
@@ -131,7 +128,7 @@ function loadArticles () {
 }
 
 
-// The first thing to do is to check if the user is logged in!
+
 loadLogin();
 
-// Now this is something that we could have directly done on the server-side using templating too!
+
