fd9a3696201474a5d16c45df1d3c025b09f1a322
shivaniii <anumulashivani94@gmail.com>
2016-11-23 20:10:06 +0530

    Update article.js

--- ui/article.js
+++ ui/article.js
@@ -1,4 +1,4 @@
-var currentArticleTitle = window.location.pathname.split('/')[2];
+var currentArticleTitle = window.location.pathname.split('/articles/')[2];
 
 function loadCommentForm () {
     var commentFormHtml = `
