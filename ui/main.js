9ed8ed6b0571dca0b97b4b9a62c9d10d0469e372
shivaniii <anumulashivani94@gmail.com>
2016-11-24 23:33:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -126,17 +126,6 @@ function loadArticles () {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
-    
-
 
 
 loadLogin();
