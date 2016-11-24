f68613596122b763c64543ff8e96f77ad40c7e8b
shivaniii <anumulashivani94@gmail.com>
2016-11-24 22:32:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,10 +53,14 @@ ${content}
 </div>
 <hr/>
              
- <div id="HCB_comment_box">Comment Form</a> is loading comments...</div>
- <link rel="stylesheet" type="text/css"
+    <h4>Comments</h4>
+             <!-- begin wwww.htmlcommentbox.com -->
+ <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Comment Form</a> is loading comments...</div>
+ <link rel="stylesheet" type="text/css" href="//www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
  <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="//www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&opts=16862&num=10&ts=1480005883438");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
-
+<!-- end www.htmlcommentbox.com -->
+              <div id="comments">
+                <center>Loading comments...</center>
               </div>
           </div>
           <script type="text/javascript" src="/ui/article.js"></script>
@@ -65,6 +69,7 @@ ${content}
     `;
     return htmlTemplate;
 }
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
