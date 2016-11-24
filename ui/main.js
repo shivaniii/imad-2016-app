dbdabf9ad5b9479e4c094836f6df454a8df1d7f2
shivaniii <anumulashivani94@gmail.com>
2016-11-24 19:19:19 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -126,6 +126,16 @@ function loadArticles () {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
+function loadCommentForm () {
+    var commentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    
 
 
 
