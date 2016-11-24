01198f27453bbf7301096db06cf747d2283dca31
shivaniii <anumulashivani94@gmail.com>
2016-11-24 19:24:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -272,6 +272,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
