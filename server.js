9e681c63efe622d6456a4a5f87bd54e3dfd912c1
shivaniii <anumulashivani94@gmail.com>
2016-11-24 23:50:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,19 +53,8 @@ ${content}
 </div>
 <hr/>
              
-   <hr/>
-         <form method='post'>
-  NAME: <input type='text' name='name' id='name' /><br />
-
-  Email: <input type='text' name='email' id='email' /><br />
-
-  Comment:<br />
-  <textarea name='comment' id='comment'></textarea><br />
-
-  <input type='hidden' name='articleid' id='articleid' value='<? echo $_GET["id"]; ?>' />
-
-  <input type='submit' value='Submit' />  
-</form>
+   <hr/> <h3>Comments</h3>
+  <div id="comment_form"/></div>
 
               <div id="comments">
                 <center>Loading comments...</center>
