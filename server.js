42627946908d902d2c7910bc4a88769a009a2d1e
shivaniii <anumulashivani94@gmail.com>
2016-11-25 19:59:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/create-user', function (req, res){
     var password = req.body.password;
       var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+    pool.query('SELECT * FROM "user" WHERE username = $1,', [username], function(err, result) {
    if (err) {
             res.status(500).send(err.stringify());
         } else {
